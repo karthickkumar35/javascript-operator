d5154f2c98be00c693995f25eaeeb05a9c90036f
karthickumar35 <karthickcs444@gmail.com>
2023-02-06 15:04:38 +0530

    update

---
+++ js/javascript.js
@@ -0,0 +1,260 @@
+//number//
+var value1=3;
+typeof(value1);
+
+
+//string//
+var value2="karthick";
+typeof(value2);
+
+
+//Null//
+var nvote=null;
+console.log(nvote);
+
+//Arithematic//
+
+var a=5;
+var b=3;
+var c=10;
+
+  //Addition//
+ 
+ console.log(a+b+c);
+ console.log(a+b+c+a+c+b);
+ 
+  //substraction//
+ 
+ console.log(a-b-c);
+ console.log(a-b-c-b-a-c);
+ 
+   //multiplication//
+ 
+ console.log(a*b*c);
+ console.log(a*b*c*a*c*b);
+ 
+ console.log(a**c);
+ console.log(c**a);
+ console.log(b**a);
+ 
+   //Divider//
+ 
+ console.log(a/b+c);
+ console.log(a/b-c*a-c)
+ 
+   //module//
+ 
+ console.log(a%b);
+ console.log(a%b*c+a/b);
+  
+ //Increment//
+ 
+   //Pre-increment//
+   
+  var a=6;
+  console.log(++a);
+  console.log(++a);
+  console.log(++a);
+  
+    //Post-increment//
+   
+  var b=8;
+  console.log(b++);
+  console.log(b++);
+  console.log(b++);
+  
+  //Decrement//
+ 
+   //Pre-decrement//
+   
+  console.log(--a);
+  console.log(--a);
+  console.log(--a);
+  
+    //Post-decrement//
+  
+  console.log(b--);
+  console.log(b--);
+  console.log(b--);
+  
+   //Comparison//
+   
+  //Relational-oparetor//
+  var x=10;
+  var y=20;
+  
+  console.log(x<y);
+  console.log(x>y);
+  console.log(x<=y);
+  console.log(x>=y);
+  
+  //Equality-oparetor//
+  let w=15;
+  let z=30;
+  let n='karthi';
+  
+  console.log(w===n);
+  console.log(w==z);
+  console.log(w!=z);
+  console.log(w!==z);
+  
+  //Assignment-oparetor// 
+  
+  var k=10;
+  console.log(k+=5);
+  console.log(k-=10);
+  console.log(k*=2);
+  console.log(k/=5);
+  console.log(k**=2);
+  console.log(k%=5);
+  
+  //logical-oparetor// 
+  
+  var p=20;
+  var q=30;
+  var r=-8;
+  var s=44;
+  
+  console.log((p<q)&&(p>r));
+  console.log((p===q)&&(p>r));
+  
+  console.log((s==r)||(s>q));
+  console.log((p<=r)||(s<=q));
+  
+  t=((s==r)||(s>q));
+  console.log(!t);
+  
+  u=((p<=r)||(s<=q));
+  console.log(!u);
+  
+  document.write(p+q-r*s);
+  document.write("<br>");
+  //Addition//
+  var a1=30;
+  var a2=35;
+  var a3=40;
+  var d=a1+a2+a3;
+  document.write(d);
+  document.write("<br>");
+  
+  //Subtraction//
+  var a1=17;
+  var a2=8;
+  var a3=4;
+  var d=a1-a2-a3;
+  document.write(d);
+  document.write("<br>");
+  
+  //Multiplication//
+  var a1=5;
+  var a2=7;
+  var a3=2;
+  var d=a1*a2*a3;
+  document.write(d);
+  document.write("<br>");
+  
+  //Division//
+  var a1=50;
+  var a2=5;
+  var a3=2;
+  var d=a1/a2/a3;
+  document.write(d);
+  document.write("<br>");
+  
+  //Area of Cricle//
+  var a1=7;
+  var a2=22/7;
+  var d=a2*a1**2;
+  document.write(d);
+  document.write("<br>");
+  
+  //Area of Triangle//
+  var a1=12;
+  var a2=14;
+  var a3=16;
+  var d=(a1+a2+a3)/2;
+  document.write(d);
+  document.write("<br>");
+  
+  //Calculate simple interest//
+  var a1=100200;
+  var a2=24;
+  var a3=50;
+  var d=(a1*a2*a3)/100;
+  document.write(d);
+  document.write("<br>");
+  
+  //Calculating Square//
+  var a1=5;
+  var b1=a1**a1;
+  document.write(b1);
+  document.write("<br>");  
+  
+  //Calculating Cube//
+  var a1=4;
+  var b1=6*a1**a1;
+  document.write(b1);
+  document.write("<br>");    
+  
+  //Calculate Total and Average//
+  var a1=34;
+  var a2=26;
+  var a3=30;
+  var d=a1+a2+a3;
+  var e=d/3;
+  document.write(d);
+  document.write("<br>");  
+  document.write(e);
+  document.write("<br>"); 
+  
+  //Larger number//
+  var a1=100;
+  var a2=12;
+  var a3=210;
+  var d=a1<a2;
+  var e=a1<a3;
+  document.write(d);
+  document.write("<br>");  
+  document.write(e);
+  document.write("<br>");
+  
+  //Positive or not//
+  var a1=33;
+  var a2=-33;
+  var d=a1>0;
+  var e=a2>0;
+  document.write(d);
+  document.write("<br>");  
+  document.write(e);
+  document.write("<br>");
+  
+  //Greater than 10 number//
+  var a1=24;
+  var a2=9;
+  var d=a1>10;
+  var e=a2>10;
+  document.write(d);
+  document.write("<br>");  
+  document.write(e);
+  document.write("<br>");
+  
+  //Odd or Even number//
+  var a1=12;
+  var a2=33;
+  var d=(a1%2)==0;
+  var e=(a2%2)==0;
+  document.write(d);
+  document.write("<br>");  
+  document.write(e);
+  document.write("<br>");
+  
+  //Number divisble by 5//
+  var a1=25;
+  var a2=37;
+  var d=(a1%5)==0;
+  var e=(a2%5)==0;
+  document.write(d);
+  document.write("<br>");  
+  document.write(e);
+  document.write("<br>");
+  
